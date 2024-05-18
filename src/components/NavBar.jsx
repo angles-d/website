00f0860c849818b2ac74de055d5f0c44256379bd
angles-d <zhjh.angela@gmail.com>
2024-05-18 11:00:12 -0400

    add vals

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,10 @@ export default function NavBar(){
     return (<>
         <div className="mt-5 row align-items-center">
             <div className="col-6 offset-6 text-start text-lg-end  justify-content-end nav-bar  d-flex">
-                <Link to="/">Home</Link>
-                <a href="/#projects" className="pl-3">Projects</a>
-                <a href="/about-me" className=" pl-3">About</a>
-                <a href="/contact" className=" pl-3" >Contact</a>
+                <Link to="./">Home</Link>
+                <a href="./#projects" className="pl-3">Projects</a>
+                <a href="./about-me" className=" pl-3">About</a>
+                <a href="./contact" className=" pl-3" >Contact</a>
                 <a href="./Dai_Resume.pdf" className="pl-3" target="_blank">Resume</a>
             </div>
         </div>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/website/"
+  base: "/website/",
+
+  // rollupOptions: {
+  //        input: {
+  //        index: fileURLToPath(new URL('index.html', import.meta.url)),
+  //        accordion: fileURLToPath(
+  //            new URL('src/components/Accordion/index.html', import.meta.url)
+  //        ),
+  //     },
+  // },
+      
 })
